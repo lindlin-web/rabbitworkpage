b48744923248567e136907eb9870f7b7df51de40
LindlinElin <114578863@qq.com>
2024-10-09 10:48:56 +0800

    fff

---
+++ homerun/Full/Facebook/js/index0.js
@@ -0,0 +1,1 @@
+function LoadCompleteFun(){console.log('LoadCompleteFun')};function GameFinish(){console.log('GameFinish')};function ClickDownLoad(){console.log('ClickDownLoad');FbPlayableAd.onCTAClick()};
---
+++ homerun/Full/Mintegral/js/index0.js
@@ -0,0 +1,1 @@
+function gameStart(){window.postMessage('gameStart','/')}function gameClose(){window.postMessage('gameClose','/')}function LoadCompleteFun(){console.log('LoadCompleteFun');window.gameReady&&window.gameReady()};function GameFinish(){console.log('GameFinish');window.gameEnd&&window.gameEnd()};function ClickDownLoad(){console.log('ClickDownLoad');window.install&&window.install()};
